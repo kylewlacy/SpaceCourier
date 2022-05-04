673c8bba4a39da79e0cff0131f215acb82a4bf85
Ashley Hutson <ashley.hutson@mheducation.com>
2022-05-03 19:26:56 -0700

    Add controller support + Restart
    (cherry picked from commit 9e12326480a00c87aeb10a78c340941a49ac9051)

--- game_over.gd
+++ game_over.gd
@@ -38,6 +38,10 @@ func _on_play_again_pressed():
 	fast_forward_input_threshold = 1
 	on_play_again.emit()
 
+func _process(delta):
+	if visible and Input.is_action_just_pressed("restart"):
+		on_play_again.emit()
+
 func _on_quit_pressed():
 	on_quit.emit()
 
