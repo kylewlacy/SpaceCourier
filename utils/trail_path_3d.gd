13bf5aed6b49c0180085d72bd08d9306abf861bd
Kyle Lacy <kylelacy@kyle.space>
2022-06-18 18:08:10 -0700

    Implement bulk of the logic for `TrailPath3D`

--- utils/trail_path_3d.gd
+++ utils/trail_path_3d.gd
@@ -1,13 +1,82 @@
 extends Object
 class_name TrailPath3D
 
-var points = PackedVector3Array()
+var transforms = PackedVector3Array()
 
-func append(point: Vector3):
-	points.append(point)
+enum Direction {START_TO_END, END_TO_START}
 
+func append(transform: Transform3D):
+	transforms.append(transform.origin)
+	transforms.append(transform.basis.x)
+	transforms.append(transform.basis.y)
+	transforms.append(transform.basis.z)
+
+@warning_ignore(integer_division)
 func size() -> int:
-	return points.size()
+	return transforms.size() / 4
 
-func get_point(i: int) -> Vector3:
-	return points[i]
+func get_transform(i: int) -> Transform3D:
+	var origin = transforms[i * 4]
+	var basis_x = transforms[i * 4 + 1]
+	var basis_y = transforms[i * 4 + 2]
+	var basis_z = transforms[i * 4 + 3]
+	return Transform3D(basis_x, basis_y, basis_z, origin)
+
+func for_each_step(space_between_points: float, start_distance: float, max_count: int, direction: Direction, f: Callable):
+	var _dbg_initial_steps = 0
+	var _dbg_trail_steps = 0
+
+	var count = 0
+
+	var i: int
+	var step: int
+	var last_i: int
+	match direction:
+		Direction.START_TO_END:
+			i = 0
+			step = 1
+			last_i = min(max_count, size())
+		Direction.END_TO_START:
+			i = size() - 1
+			step = -1
+			last_i = 0
+
+	var distance = 0.0
+	var last_transform = get_transform(i)
+
+	while distance < start_distance:
+		var transform = get_transform(i)
+		distance += last_transform.origin.distance_to(transform.origin)
+
+		i += step
+		_dbg_initial_steps += 1
+		if i == last_i:
+			print("early initial exit: %s + %s steps" % [_dbg_initial_steps, _dbg_trail_steps])
+			return
+
+		last_transform = transform
+
+	f.call(last_transform)
+	count += 1
+
+	while count < max_count:
+		distance = -1.0
+		while distance < space_between_points:
+			if distance < 0.0:
+				distance = 0.0
+
+			var transform = get_transform(i)
+			distance += last_transform.origin.distance_to(transform.origin)
+
+			i += step
+			_dbg_trail_steps += 1
+			if i == last_i:
+				print("early exit: %s + %s steps" % [_dbg_initial_steps, _dbg_trail_steps])
+				return
+
+			last_transform = transform
+
+		f.call(last_transform)
+		count += 1
+
+	print("finished: %s + %s steps" % [_dbg_initial_steps, _dbg_trail_steps])
--- utils/trail_path_3d_test.gd
+++ utils/trail_path_3d_test.gd
@@ -5,14 +5,15 @@ var trail: TrailPath3D = TrailPath3D.new()
 
 var ship_curve_debug_mesh: ImmediateMesh
 
-@export
-var speed = 10
+@export var speed = 10
+@export var steps_per_phys_frame = 100
 
-@export
-var steps_per_phys_frame = 100
+@export var debug_trail_size = 1000
+@export var debug_trail_initial_offset = 0.1
+@export var debug_trail_offset = 0.01
 
-@export
-var debug_trail_size = 1000
+
+var last_transform = null
 
 func _ready():
 	ship_curve_debug_mesh = ImmediateMesh.new()
@@ -30,12 +31,34 @@ func _process(_delta):
 	ship_curve_debug_mesh.clear_surfaces()
 	ship_curve_debug_mesh.surface_begin(Mesh.PRIMITIVE_POINTS)
 
-	var max_points_to_draw = debug_trail_size if debug_trail_size > 0 else trail.size()
-	for i in range(0, min(trail.size(), max_points_to_draw)):
-		ship_curve_debug_mesh.surface_add_vertex(trail.get_point(trail.size() - i - 1))
+	var max_points_to_draw = debug_trail_size if debug_trail_size > 0 else trail.size()#
+
+	var state = {"points_drawn": 0}
+	var for_each_transform = func(transform: Transform3D):
+		ship_curve_debug_mesh.surface_add_vertex(transform.origin)
+		state["points_drawn"] += 1
+	trail.for_each_step(debug_trail_offset, debug_trail_initial_offset, max_points_to_draw, TrailPath3D.Direction.END_TO_START, for_each_transform)
+
+	if state["points_drawn"] > max_points_to_draw:
+		push_warning("Expected to draw at most %s but drew %s points" % [max_points_to_draw, state["points_drawn"]])
+
+
 	ship_curve_debug_mesh.surface_end()
 
 func step(delta):
 	$Node3D/Path3D/PathFollow3D.offset += delta * speed;
 
-	trail.append($Node3D/Path3D/PathFollow3D/MeshInstance3D.global_transform.origin)
+	var new_transform = $Node3D/Path3D/PathFollow3D/MeshInstance3D.global_transform
+	trail.append(new_transform)
+
+	var check_new_transform = trail.get_transform(trail.size() - 1)
+	if check_new_transform != new_transform:
+		push_warning("Transform did not match!")
+
+	if last_transform:
+		var check_last_transform = trail.get_transform(trail.size() - 2)
+		if check_last_transform != last_transform:
+			push_warning("Last transform did not match!")
+
+	last_transform = new_transform
+
